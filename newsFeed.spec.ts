a56976162e36bbac3ccf28557da48694ee1a88c0
Sébastien M <90312970+sebastienwolf@users.noreply.github.com>
2026-09-03 12:13:47 +0200

    Refactor view feed test for clarity and structure

--- newsFeed.spec.ts
+++ newsFeed.spec.ts
@@ -5,7 +5,6 @@ import { waitForLoadingToSkeleton } from '../loader';
 test('view feed', async ({ page }) => {
   await login({ page });
   await page.getByRole('link', { name: 'Accueil' }).click();
-  await page.waitForTimeout(15000);
   await waitForLoadingToSkeleton({ page });
   await expect(page.getByRole('button', { name: 'Ajouter une annonce' })).toBeVisible();
   await page.getByRole('button', { name: 'Ajouter une annonce' }).click();
@@ -18,7 +17,6 @@ test('view feed', async ({ page }) => {
     page.getByRole('link', { name: 'Demander un service', exact: true })
   ).toBeVisible();
 
-  // Vérifie qu'il y a au moins 4 cartes dans le feed
   const cards = page.locator('.masonry-wall .masonry-item');
   const count = await cards.count(); await page.goto('http://localhost:3000/feed');
   expect(count).toBeGreaterThanOrEqual(4);
